138ec1f551e70f3ffc74d41d8cae4d48e87875b9
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-04-21 12:27:02 -0700

    finished

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -9,6 +9,23 @@ const Update = () => {
         const name = form.name.value;
         const email = form.email.value;
         console.log(name,email);
+        const user = {name,email};
+        fetch(`http://localhost:5000/users/${loadUser._id}`,{
+           
+        method:"PUT",
+        headers:{
+            'content-type':'application/json',
+        },
+        body:JSON.stringify(user)
+
+
+
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            alert("updated successfully")
+            console.log(data);
+        })
     }
     return (
         <div>
